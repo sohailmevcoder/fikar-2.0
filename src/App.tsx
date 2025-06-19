f7c0d3aafbe661d99097bce0b10dccd4c46a3ec5
mragarwalji <krishagarwal8962@gmail.com>
2025-06-19 13:19:02 +0530

    ryr

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
+
+
 import PatientLogin from "./pages/PatientLogin";
 import PatientSignp from "./pages/PatientSignup"
 import UserDashboard from "./pages/UserDashboard";
@@ -34,8 +36,17 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+
+          <Route path="/patient-login" element = {<PatientLogin/>} />
+          <Route path="/patient-signup" element = {<PatientSignp/>} />
           <Route path="/user-dashboard" element={<UserDashboard />} />
+
+          <Route path="/doctor-login" element = {<DoctorLogin/>} />
+          <Route path="/doctor-signup" element = {<DoctorSignup/>} />
           <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+
+          <Route path="/clinic-admin-login" element = {<ClinicAdminLogin/>} />
+          <Route path="/admin-signup" element = {<ClinicAdminSignup/>} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
